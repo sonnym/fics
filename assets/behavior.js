0f730f59563dfdaffd666ec316372d821835e397
sonnym <michaud.sonny@gmail.com>
2014-01-01 21:55:58 -0500

    Generated documentation for 008541801378ea5fc4c694059b98b27c68d22775

--- assets/behavior.js
+++ assets/behavior.js
@@ -223,14 +223,6 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                   "type": "heading",
                   "data": {
                     "level": 3,
-                    "title": "awaitNext",
-                    "slug": "awaitnext"
-                  },
-                  "depth": 3
-                }, {
-                  "type": "heading",
-                  "data": {
-                    "level": 3,
                     "title": "lines",
                     "slug": "lines"
                   },
@@ -462,14 +454,6 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                 "level": 3,
                 "title": "sought",
                 "slug": "sought"
-              },
-              "depth": 3
-            }, {
-              "type": "heading",
-              "data": {
-                "level": 3,
-                "title": "awaitNext",
-                "slug": "awaitnext"
               },
               "depth": 3
             }, {
