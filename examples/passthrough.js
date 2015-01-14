bef00a6b7071a3e4de6e2d51ceaf8983cc03ab67
sonnym <michaud.sonny@gmail.com>
2015-01-14 14:31:39 -0500

    add example repl

---
+++ examples/passthrough.js
@@ -0,0 +1,12 @@
+/**
+ * A very simple example that provides a connection to the FICS server as a
+ * REPL interface. This pipes stdin from the process into the socket and
+ * the socket into stdout.
+ */
+var FicsClient = require("./../");
+var socket = (new FicsClient()).getSocket();
+
+process.stdin.pipe(socket);
+socket.pipe(process.stdout);
+
+socket.on("close", process.exit);
