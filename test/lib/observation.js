41f9250e813e244251a5d87155ec3e7dfe7b6779
sonnym <michaud.sonny@gmail.com>
2015-01-22 16:59:35 -0500

    support ratings of hyphens

--- test/lib/observation.js
+++ test/lib/observation.js
@@ -13,7 +13,7 @@ exports.testGames = function(test) {
   var fics = new FICSClient();
 
   fics.games().then(function(games) {
-    test.equal(319, games.length);
+    test.equal(324, games.length);
 
     mockSocket.close();
   });
